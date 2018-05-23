78776de2e9376de0cf6b253ef352083bd023c311
sksergey <skovorodkinsergey86@gmail.com>
2018-05-23 16:24:31 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LogInComponent,
+    SignUpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+      RouterModule.forRoot([
+          { path: 'log-in', component: LogInComponent },
+          { path: 'sign-up', component: SignUpComponent },
+          { path: '', component: LandingComponent },
+          { path: '**', redirectTo: '/' }
+      ]),
+      FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/log-in/log-in.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../../models/user';
+
+@Component({
+  selector: 'app-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.css']
+})
+export class LogInComponent implements OnInit {
+
+  user = new User();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(): void {
+    console.log(this.user);
+  }
+
+}
